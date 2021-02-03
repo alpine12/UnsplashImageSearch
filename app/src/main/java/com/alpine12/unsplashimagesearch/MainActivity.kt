71087cf45dfd7bcf7d8895a374e3dfce4561d863
alpine12 <iqballecip@gmail.com>
2021-02-03 20:09:04 +0700

    Initial commit

---
+++ app/src/main/java/com/alpine12/unsplashimagesearch/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.alpine12.unsplashimagesearch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
