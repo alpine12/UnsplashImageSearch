072caec74f513c39c3bc842f92141589eb4b280e
alpine12 <iqballecip@gmail.com>
2021-02-11 22:12:30 +0700

    Search functionality

--- app/src/main/java/com/alpine12/unsplashimagesearch/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/alpine12/unsplashimagesearch/ui/gallery/GalleryFragment.kt
@@ -2,7 +2,10 @@ package com.alpine12.unsplashimagesearch.ui.gallery
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.alpine12.unsplashimagesearch.R
@@ -27,8 +30,8 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
         binding.apply {
             recyclerView.setHasFixedSize(true)
             recyclerView.adapter = adapter.withLoadStateHeaderAndFooter(
-                header = UnsplashPhotoLoadStateAdapter {adapter.retry()},
-                footer = UnsplashPhotoLoadStateAdapter {adapter.retry()}
+                header = UnsplashPhotoLoadStateAdapter { adapter.retry() },
+                footer = UnsplashPhotoLoadStateAdapter { adapter.retry() }
             )
         }
 
@@ -36,6 +39,31 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
             adapter.submitData(viewLifecycleOwner.lifecycle, it)
             Log.d("Fragment", "onViewCreated: ${adapter.itemCount}  , $it")
         }
+
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_gallery, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+                searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                if (p0 != null) {
+                    binding.recyclerView.scrollToPosition(0)
+                    viewModel.searchPhotos(p0)
+                    searchView.clearFocus()
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                return true
+            }
+        })
     }
 
     override fun onDestroyView() {
