678d6393cde9511cfdfd2383a0102727f2f3ed1d
alpine12 <iqballecip@gmail.com>
2021-02-12 10:21:37 +0700

    Handling process death

--- app/src/main/java/com/alpine12/unsplashimagesearch/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/alpine12/unsplashimagesearch/ui/gallery/GalleryViewModel.kt
@@ -1,17 +1,21 @@
 package com.alpine12.unsplashimagesearch.ui.gallery
 
+import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.switchMap
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
 import com.alpine12.unsplashimagesearch.data.UnsplashRepository
 
-class GalleryViewModel @ViewModelInject constructor(private val repository: UnsplashRepository) :
+class GalleryViewModel @ViewModelInject constructor(
+    private val repository: UnsplashRepository,
+    @Assisted state: SavedStateHandle
+) :
     ViewModel() {
 
-    private val currentQuery = MutableLiveData(DEFFAULT_QUERY)
+    private val currentQuery = state.getLiveData(CURRENT_QUERY, DEFAULT_QUERY)
 
     val photo = currentQuery.switchMap { queryString ->
         repository.getSearchResult(queryString).cachedIn(viewModelScope)
@@ -22,7 +26,8 @@ class GalleryViewModel @ViewModelInject constructor(private val repository: Unsp
     }
 
     companion object {
-        private const val DEFFAULT_QUERY = "cats"
+        private const val CURRENT_QUERY = "current_query"
+        private const val DEFAULT_QUERY = "cats"
     }
 
     init {
